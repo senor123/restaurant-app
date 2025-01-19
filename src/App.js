ae72862b4f11cc9514a47ea1eaa6435402b4b760
Pardha Saradhi Avula <pardhuavula@gmail.com>
2025-01-19 07:28:47 +0000

    New update

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import './App.css'
+import {Switch, Route} from 'react-router-dom'
 import Home from './components/Home'
 // write your code here
-const App = () => <Home />
+const App = () => (
+  <Switch>
+    <Route exact path="/" component={Home} />
+  </Switch>
+)
 
 export default App
